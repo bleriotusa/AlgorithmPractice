90a350d48acf0f8a293f1dd7eb536eea9c5a40ef
Michael J. Chen <bleriotusa@gmail.com>
2018-03-09 20:15:41 -0800

    isHeightBalanced spacing issue merge

--- ElementsProgrammingInterviews/IsHeightBalancedAlternate.py
+++ ElementsProgrammingInterviews/IsHeightBalancedAlternate.py
@@ -10,6 +10,7 @@ class BinaryTreeNode:
         return '%s <- %s -> %s' % (self.left and self.left.data, self.data,
                                    self.right and self.right.data)
 
+
 def is_height_balanced(root: BinaryTreeNode):
     def is_height_balanced_helper(root: BinaryTreeNode):
         if not root:
@@ -20,9 +21,10 @@ def is_height_balanced(root: BinaryTreeNode):
         if left < 0 or right < 0 or abs(right - left) > 1:
             return -1
 
-        return max(left+1, right+1)
+        return max(left + 1, right + 1)
 
     return is_height_balanced_helper(root) > 0
+
 
 tree = BinaryTreeNode()
 tree.left = BinaryTreeNode()
@@ -32,7 +34,6 @@ tree.right.left = BinaryTreeNode()
 tree.right.right = BinaryTreeNode()
 print(is_height_balanced(tree))
 assert is_height_balanced(tree)
-print(is_height_balanced(tree))
 # Non-balanced binary tree test.
 tree = BinaryTreeNode()
 tree.left = BinaryTreeNode()
